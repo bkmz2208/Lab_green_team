92ff8e6034db4a66c3f155765704ca823da83684
FedorenkoAnna <feanneta@gmail.com>
2020-10-15 10:10:18 +0300

    update BlackFactory

---
+++ src/com/company/BlackFactory.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class BlackFactory extends BaseFactory { // WhiteFactory from BaseFactory
+
+    @Override
+    //create a black circle(radius)
+    public Circle createCircle(int radius) {
+        return new BlackCircle(radius);
+    }
+
+    @Override
+    //create a white triangle (x1,y1); (x2,y2); (x3,y3)
+    public Triangle createTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+        return new BlackTriangle(x1, y1, x2, y2, x3, y3);
+    }
+
+    @Override
+    public String toString() {
+        return "BlackFactory";
+    }
+
+    @Override
+    public BaseFactory getFactory() {
+        return this;
+    }
+}
