e1b5d5f474b80a6d1c1b217bb61dd38b2f32880f
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2020-10-16 17:37:36 +0300

    updated main

--- src/com/company/AbstractFactory.java
+++ src/com/company/AbstractFactory.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public interface AbstractFactory {
-    public static BaseFactory getFactory(String string) {
+     static BaseFactory getFactory(String string) {
         if (string.equalsIgnoreCase("white"))
             return new WhiteFactory();
         else return new BlackFactory();
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,16 @@ package com.company;
 public class Main {
     public static void main(String []args){
 
-        BaseFactory whiteFactory = new WhiteFactory();
-        BaseFactory blackFactory = new BlackFactory();
-       Circle blackCircle = blackFactory.createCircle(3);
+        System.out.println("---------------------");
+        BaseFactory whiteFactory = AbstractFactory.getFactory("white");
+        BaseFactory blackFactory = AbstractFactory.getFactory("black");
+        Circle blackCircle = blackFactory.createCircle(3);
+        Circle whiteCircle = whiteFactory.createCircle(4);
+        System.out.println("--------BLACK CIRCLE------");
         System.out.println(blackCircle);
-
+        System.out.println("--------WHITE CIRCLE------");
+        System.out.println(whiteCircle);
+        System.out.println();
 
     }
 }
