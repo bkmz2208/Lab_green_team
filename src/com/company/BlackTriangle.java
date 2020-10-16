dfaac3b4098e4d7cd468386b575371c2f3c866d3
anyayarmola <anna16yarmola@gmail.com>
2020-10-16 15:28:21 +0300

    исправление BlackTriangle

--- src/com/company/BlackTriangle.java
+++ src/com/company/BlackTriangle.java
@@ -2,9 +2,6 @@ package com.company;
 
 public class BlackTriangle extends Triangle {
 
-    public BlackTriangle() {
-    }
-
     public BlackTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
         super(x1, y1, x2, y2, x3, y3);
     }
