6eb337a95c7b8841cfbe500c52e0ad1de5fbca8d
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2020-10-16 18:16:34 +0300

    final changes to Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,19 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String []args){
+    public static void main(String []args)throws Exception{
 
         System.out.println("---------------------");
         BaseFactory whiteFactory = AbstractFactory.getFactory("white");
         BaseFactory blackFactory = AbstractFactory.getFactory("black");
-
+        whiteCircleCreation(whiteFactory, 4);
         System.out.println();
-
+        blackCircleCreation(blackFactory, 2);
+        System.out.println("---------------------");
+        whiteTriangleCreation(whiteFactory, 1,2,0,0,2,-1);
+        System.out.println();
+        blackTriangleCreation(blackFactory, 2,1,0,0,-1,-2);
 
     }
 
@@ -28,4 +31,16 @@ public class Main {
         System.out.println("Area of black circle: "+ blackCircle.areaCalculation());
     }
 
+    public static void whiteTriangleCreation(BaseFactory factory, int X1, int Y1, int X2, int Y2, int X3, int Y3) throws Exception {
+        Triangle whiteTriangle = factory.createTriangle(X1,X2,X3,Y1,Y2,Y3);
+        System.out.println("--------WHITE TRIANGLE------");
+        System.out.println(whiteTriangle);
+        System.out.println("Area of white triangle: "+ whiteTriangle.areaCalculation());
+    }
+    public static void blackTriangleCreation(BaseFactory factory, int X1, int Y1, int X2, int Y2, int X3, int Y3) throws Exception {
+        Triangle blackTriangle = factory.createTriangle(X1,X2,X3,Y1,Y2,Y3);
+        System.out.println("--------BLACK TRIANGLE------");
+        System.out.println(blackTriangle);
+        System.out.println("Area of black triangle: "+ blackTriangle.areaCalculation());
+    }
 }
